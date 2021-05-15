ffe52f6eb7b037c1d8ace5138b30ead8017f10ec
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-05-15 21:12:18 +0300

    find

--- contacts.js
+++ contacts.js
@@ -1,27 +1,91 @@
-import fs from "fs/promises";
-import path from "path";
-import { isAccessible } from "./lib/accesible.js";
+//=================================================================
 
-class SortFile {
-  constructor(dist) {
-    this.dist = dist;
-  }
-  async #copyFile(file) {
-    const folder = path.extname(file.path);
-    const targetPath = path.join(this.dist, folder);
-  }
-  async #readFolder(base) {
-    const files = await fs.readdir(base);
-    for (const item of files) {
-      const localBase = path.join(base, item);
-      const state = await fs.stat(localBase);
-      if (state.isDirectory()) {
-        await this.readFolder(localBase);
-      } else {
-        await this.#copyFile({ name: item, path: localBase });
-      }
+// contacts.js
+
+let fs = require("fs"),
+  path = require("path"),
+  util = require("util");
+let content;
+console.log(content);
+
+//  Раскомментируй и запиши значение
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+console.log(contactsPath);
+// function (err, data) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+//   let content = util.format(JSON.parse(data)); // старый вариант data, "test", "test", "test"
+//   console.log(content);
+
+// for (var key in data) {
+//   console.log(key.email);
+// }
+
+// let jsonContent = JSON.parse(content);
+// console.log(jsonContent);
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+
+  const fs = require("fs");
+
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  }
+    const array = JSON.parse(data);
+    const res = array.map((el) => el);
+    console.table(res);
+  });
 }
 
-export default SortFile;
+function getContactById(contactId) {
+  // ...твой код
+  const fs = require("fs");
+
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const array = JSON.parse(data);
+    console.log(array);
+    const find = array.filter(function (item, i, arr) {
+      console.log(item.id === i);
+    });
+    // var omsk = filterByCity("2");
+  });
+  // function filterByCity(arr, city) {
+  //   return arr.filter(function (item, i, arr) {
+  //     return item.city == city;
+  //   });
+  // }
+
+  // var omsk = filterByCity(myArray, "Омск");
+
+  // console.log(omsk);
+
+  console.log("ту-ту-ту");
+}
+
+function removeContact(contactId) {
+  // ...твой код
+  console.log("Работате ");
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+  console.log("Работает");
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  contactsPath,
+};
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hi there!");
+// import fs from "fs/promises";
+// import path from "path";
+
+// let fs = require("fs"),
+//   path = require("path"),
+//   util = require("util");
+// let content;
+// console.log(content);
+
+// const contactsPath = fs.readFile(
+//   path.join(__dirname, "db", "contacts.json"),
+//   "utf8",
+//   function (err, data) {
+//     if (err) {
+//       console.log(err);
+//       process.exit(1);
+//     }
+//     content = util.format(data, "test", "test", "test");
+//     console.log(content);
+//   }
+// );
+let contactsRediness = require("./contacts");
+//===========================
+
+console.log(contactsRediness.listContacts()); // => 'function'
+console.log(contactsRediness.getContactById()); // => 'function'
+console.log(contactsRediness.removeContact()); // => undefined
+console.log(contactsRediness.addContact()); // => undefined
+//console.log(contactsRediness);
+//JSON.parse(data);
